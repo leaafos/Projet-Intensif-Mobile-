39a18ab3848841bfa884b053b2ce39473f3de87b
leaafos <leafostacz@gmail.com>
2024-04-16 15:41:57 +0200

    code clean qui marche

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './styles/App.css';
+
+//Ici c'est notre home page 
+
+//là on va mettre un bouton pour aller sur une autre page 
+//une page produit avec un bouton 
+//Le bouton il va vers les annonces 
+
+
+
+/*
+Toutes les pages : 
+Home page 
+Page produit 
+Page annonce 
+Page liste annonces 
+Page profil 
+Page messagerie 
+*/
