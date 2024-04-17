60fb8f1a7b1222897fc53f1fa574cc3ba24fb833
leaafos <leafostacz@gmail.com>
2024-04-17 08:06:29 +0200

    homepage qui s'affiche !!
    changement des versions dans package.json pour des versions plus stables et non expérimentables, réécriture des routes

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Routes from './routes.jsx';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+/*ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+)*/
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Routes />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
--- vite-project/src/routes.jsx
+++ vite-project/src/routes.jsx
@@ -1,6 +1,6 @@
-import App from "./App.jsx";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import HomePage from "./pages/HomePage.jsx";
-
+/*
 export default [
     {
         path: "/",
@@ -10,4 +10,17 @@ export default [
         ]
 
     }
-]
+]*/
+function Routes () {
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/" component={HomePage}/>
+                
+            </Switch>
+        </Router>
+    )
+}
+
+export default Routes;
+
