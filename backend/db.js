74a7e777ba14fa732df45c4186592da2e214c19b
leaneleclercq <l_leclercq4@etu-webschoolfactory.fr>
2024-04-17 17:42:20 +0200

    rectification de karl

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const knex = require("knex")({
     client: "sqlite3", // or 'better-sqlite3'
     connection: {
-      filename: "../data.sqlite",
+      filename: "./data.sqlite",
     },
     useNullAsDefault: true,
   });
