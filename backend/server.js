afed6711c0509755869a179d9734340ae4ae308e
leaafos <leafostacz@gmail.com>
2024-04-17 12:45:36 +0200

    back

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(async (req, res, next) => {
+  if (req.headers.authorization) {
+    req.user = await User.getUserById(parseInt(req.headers.authorization));
+  }
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.use(require("./routes/security"));
+
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
+
+
+
+
+// scripts start node server.js name server version 1.0.0
+//express ne fait que du routing mais on peut lui mettre des logiques metiers middle where ???
--- src/components.js/Bouton.jsx
+++
@@ -1,5 +0,0 @@
-import React, { useEffect, useState } from 'react';
-
-
-//ici ça sera un bouton qu'on exportera dans les autres pages pour l'utiliser ailleurs 
-export default Bouton 
