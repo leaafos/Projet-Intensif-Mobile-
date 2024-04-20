f916f97e034e3edf670b363a0a4edaa70ba1694d
leaneleclercq <l_leclercq4@etu-webschoolfactory.fr>
2024-04-20 22:37:00 +0200

    font en place

--- frontend/src/pages/Produit.jsx
+++ frontend/src/pages/Produit.jsx
@@ -2,25 +2,35 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/produit.css'
 import Logo from '../assets/LOGO.svg';
+import narratif from '../assets/narratif.svg';
+import aventure from '../assets/aventure.svg';
 import BurgerMenu from '../components/BurgerMenu';
+import unleaving from '../assets/unleaving-produit.png';
+import cloche from '../assets/cloche.svg';
+import favoris from '../assets/favoris.svg';
 
 
 export default function Produit() {
     return (
         <div className="produit">
-            <div className='header'>
+            <div className="header">
                 <img src={Logo} alt="Logo"></img>
-                <BurgerMenu></BurgerMenu>
             </div>
-            <div className="titre">
-                <h4>UNLEAVING</h4>
-c            </div>
-            <Link to="/annonces">
-                <button>Voir les annonces </button>
-            </Link>
-            <Link to="/">
-                <button>Retour au menu </button>
-            </Link>
+            <div>
+                <h4 className="titreJeu">UNLEAVING</h4>
+                <img src={cloche} className="cloche"></img>
+                <img src={favoris} className="favoris"></img>
+            </div>
+            <div>
+                <div className='categorie1'>
+                    <img src={narratif}></img>
+                    <span className='titreCategorie1'>Narratif</span>
+                </div>
+                <div className='categorie2'>
+                    <img src={aventure}></img>
+                    <span className='titreCategorie2'>Aventure</span>
+                </div>
+            </div>
         </div>
     )
 }
