44631a26e4d3c89641459acd8c22b392d64cf6b2
leaafos <leafostacz@gmail.com>
2024-04-20 20:58:07 +0200

    swipe terminé

--- frontend/src/pages/Swipe.jsx
+++ frontend/src/pages/Swipe.jsx
@@ -10,15 +10,19 @@ import Card1Back from '../assets/card1back.png';
 import Card2 from '../assets/card2-front.png';
 import Card2Back from '../assets/card2-back.png';
 import Card3 from '../assets/card3.png';
-import Card3Back from '../assets/card3-back.png'
+import Card3Back from '../assets/card3-back.png';
+import Card4 from '../assets/Card4.png';
 
 
 const Swipe= () => {
     const [isFlipped, setIsFlipped] = useState(false);
     const [isExiting, setIsExiting] = useState(false);
-    const [showNewCard, setShowNewCard] = useState(false);
-    const [showLastCard, setShowLastCard] = useState(false);
+    const [showSecond, setShowSecondCard] = useState(false);
+    const [ShowThirdCard, setShowThirdCard] = useState(false);
     const [exitSecondCard, setExitSecondCard] = useState(false);
+    const [ShowFourthCard, setShowFourthCard] = useState(false);
+    const [exitThirdCard, setExitThirdCard] = useState(false);
+    const [clickCount, setClickCount] = useState(0);
 
     const handleClick = () => {
         setIsFlipped(!isFlipped);
@@ -27,13 +31,18 @@ const Swipe= () => {
     const handlePoubelleClick = () => {
         console.log('Bouton "Poubelle" cliqué');
         setIsExiting(true);
-        setShowNewCard(true);
+        setShowSecondCard(true);
     };
 
     const handleEnregistrerClick = () => {
+        setClickCount(prevCount => prevCount + 1);
         console.log('Bouton "Enregistrer" cliqué');
         setExitSecondCard(true);
-        setShowLastCard(true);
+        setShowThirdCard(true);
+        if (clickCount == 1) {
+            setShowFourthCard(true);
+            setExitThirdCard(true);
+        };
     };
     
   
@@ -46,7 +55,7 @@ const Swipe= () => {
 
 
         </div>
-            <div className={`card ${isFlipped ? 'flipped' : ''} ${isExiting ? 'exit' : ''}`} >
+            <div className={`card ${isFlipped ? 'flipped' : ''} ${isExiting ? 'exitLeft' : ''}`} >
                 <div className='card-content-front'onClick={handleClick}>
                     <img src={Card1}></img>
                 </div>
@@ -55,8 +64,8 @@ const Swipe= () => {
                 </div>
             </div>
 
-        {showNewCard && (
-            <div className={`new-card ${showNewCard ? 'enter-from-right' : ''} ${isFlipped ? 'flipped' : ''} ${exitSecondCard ? 'exit' : ''}`}>
+        {showSecond && (
+            <div className={`new-card ${showSecond ? 'enterRight' : ''} ${isFlipped ? 'flipped' : ''} ${exitSecondCard ? 'exitRight' : ''}`}>
                 <div className='card2-content-front'onClick={handleClick}>
                     <img src={Card2}></img>
                 </div>
@@ -66,8 +75,8 @@ const Swipe= () => {
             </div>
         )}
 
-        {showLastCard && (
-            <div className={`last-card ${showLastCard ? 'enter-from-right' : ''} ${isFlipped ? 'flipped' : ''}`}>
+        {ShowThirdCard && (
+            <div className={`last-card ${ShowThirdCard ? 'enterLeft' : ''} ${isFlipped ? 'flipped' : ''} ${exitThirdCard ? 'exitRight' : ''}`}>
                 <div className='card3-content-front'onClick={handleClick}>
                     <img src={Card3}></img>
                 </div>
@@ -75,6 +84,11 @@ const Swipe= () => {
                     <img src={Card3Back}></img>
                 </div>
             </div>
+        )}
+        {ShowFourthCard && (
+                <div className={`fourth-card ${ShowFourthCard ? 'enterLeft' : ''}`}>
+                    <img src={Card4}></img>
+                </div>
         )}
 
         <div className='boutons'>
