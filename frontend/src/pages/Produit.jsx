3525591101b8cc65247fe7a188af8e7ee5fe84a9
leaneleclercq <l_leclercq4@etu-webschoolfactory.fr>
2024-04-21 17:22:46 +0200

    scroll qui marche enfin

--- frontend/src/pages/Produit.jsx
+++ frontend/src/pages/Produit.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import '../styles/produit.css'
+import '../styles/produit.css';
 import Logo from '../assets/LOGO.svg';
 import narratif from '../assets/narratif.svg';
 import aventure from '../assets/aventure.svg';
-import BurgerMenu from '../components/BurgerMenu';
-import unleaving from '../assets/unleaving-produit.png';
 import cloche from '../assets/cloche.svg';
 import favoris from '../assets/favoris.svg';
 import developpeur from '../assets/developpeur.png';
@@ -19,93 +16,87 @@ import jeu1 from '../assets/jeu1.png';
 import jeu2 from '../assets/jeu2.png';
 import jeu3 from '../assets/jeu3.png';
 
-
-
 export default function Produit() {
     return (
-        <div className="produit">
-            <div className="header">
-                <img src={Logo} alt="Logo"></img>
+        <div className="produit-container">
+            <div className="produit-header">
+                <img src={Logo} alt="Logo" className="produit-logo" />
             </div>
-            <div>
-                <h4 className="titreJeu">UNLEAVING</h4>
-                <img src={cloche} className="cloche"></img>
-                <img src={favoris} className="favoris"></img>
-            </div>
-            <div>
-                <div className='categorie1'>
-                    <img src={narratif}></img>
-                    <span className='titreCategorie1'>Narratif</span>
+            <div className="produit-content">
+                <div className="produit-title-section">
+                    <h4 className="produit-titreJeu">UNLEAVING</h4>
+                    <div className="produit-actions">
+                        <img src={cloche} className="produit-cloche" alt="Cloche" />
+                        <img src={favoris} className="produit-favoris" alt="Favoris" />
+                    </div>
                 </div>
-                <div className='categorie2'>
-                    <img src={aventure}></img>
-                    <span className='titreCategorie2'>Aventure</span>
+
+                <div className="produit-categories">
+                    <div className="produit-categorie1">
+                        <img src={narratif} alt="Narratif" />
+                        <span className="produit-titreCategorie1">Narratif</span>
+                    </div>
+                    <div className="produit-categorie2">
+                        <img src={aventure} alt="Aventure" />
+                        <span className="produit-titreCategorie2">Aventure</span>
+                    </div>
                 </div>
-                <div>
-                    <span className='texte1'>Développeur :</span>
-                    <span className='texte2'>Dream Harvest</span>
-                    <img src={developpeur} className="icondev"></img>
+
+                <div className="produit-info">
+                    <span className="produit-texte1"><b>Développeur :</b> Dream Harvest</span>
+                    <img src={developpeur} alt="Développeur" className="produit-icondev" />
                 </div>
-                <div>
-                    <span className='texte3'>Date de sortie :</span>
-                    <span className='texte4'>04/05/23</span>
+
+                <div className="produit-info">
+                    <span className="produit-texte2"><b>Date de sortie :</b> 04/05/23</span>
                 </div>
-                <div>
-                    <span className='texte5'>Espace disque :</span>
-                    <span className='texte6'>12,4 GB</span>
+                <div className="produit-info">
+                    <span className="produit-texte3"><b>Espace disque :</b> 12,4 GB</span>
                 </div>
-                <div className="separation"></div>
-                <div>
-                    <span className="texte7">Prix <b>Dream Harvest</b></span>
+                <div className="produit-separation"></div>
+
+                <div className="produit-prix-section">
+                    <span className="produit-texte4">Prix <b>Dream Harvest</b></span>
                     <span className="prix">16,78€</span>
                 </div>
-                <div>
-                    <span className='texte8'>autres offres</span>
-                    <img src={icon1} className="icon1"></img>
-                    <img src={icon2} className="icon2"></img>
-                    <img src={icon3} className="icon3"></img>
-                    <img src={icon4} className="icon4"></img>
-                    <img src={icon5} className="icon5"></img>
-                    <img src={plus} className="plus"></img>
+                <div className="ligne1">
+                    <span className='produit-texte5'>autres offres</span>
+                    <div className="produit-offres">
+                        <img src={icon1} alt="Icon 1" />
+                        <img src={icon2} alt="Icon 2" />
+                        <img src={icon3} alt="Icon 3" />
+                        <img src={icon4} alt="Icon 4" />
+                        <img src={icon5} alt="Icon 5" />
+                        <img src={plus}  alt="Plus"/>
+                    </div>
                 </div>
-                <div className="separation1"></div>
+                <div className="produit-separation"></div>
                 <div>
-                    <span className="texte9"><b>Description</b></span>
-                    <span className="texte10">Galerie</span>
-                    <span className="texte11">Prix</span>
-                    <span className="texte12">Développeur</span>
+                    <span className="produit-texte6"><b>Description</b></span>
+                    <span className="produit-texte7">Galerie</span>
+                    <span className="produit-texte8">Prix</span>
+                    <span className="produit-texte9">Développeur</span>
                 </div>
                 <div className="ligne"></div>
-                <div>
-                    <span className="texte13"><b>ILS PARLENT DE NOUS</b></span>
+                <span className="produit-texte10">ILS PARLENT DE NOUS</span>
+                <div className="produit-review">
+                    <img src={jeu1} alt="Jeu 1" className="produit-jeu" />
+                    <img src={jeu2} alt="Jeu 2" className="produit-jeu" />
                 </div>
-                <div>
-                    <img src={jeu1} className="jeu1"></img>
-                    <img src={jeu2} className="jeu2"></img>                
-                </div>
-                <div>
-                    <span className="texte14">@2santos.amelya</span>
-                    <span className="texte15">“Une exploration des recoins obscurs de l'esprit humain à travers des énigmes stimulantes.”</span>
-                    <span className="texte16">@talmo.primesupreme</span>
-                    <span className="texte17">“Un monde surréaliste rendu vivant par des œuvres d'art peintes à la main.”</span>
-                </div>
-                <div>
-                    <span className="texte18">
-                    UNLEAVING est un jeu de plateforme et de réflexion captivant, doté d'un monde surréaliste rendu vivant par des œuvres d'art peintes à la main et des animations de peinture uniques. 
-                    
-                    Les énigmes du jeu et les défis basés sur les compétences sont étroitement intégrés dans la trame de l'histoire, permettant une expérience immersive.
 
-                    Qui es-tu ? 
-                    Que fais-tu ici ? 
-                    Tu es perdu. 
-                    Pourtant, tu es chez toi. 
-                    Ne vacille pas.
-                    Car tu es revenu.
+                <div className="produit-temoignages">
+                    <span className="produit-texte">@2santos.amelya</span>
+                    <span className="produit-texte">“Une exploration des recoins obscurs de l'esprit humain à travers des énigmes stimulantes.”</span>
+                    <span className="produit-texte">@talmo.primesupreme</span>
+                    <span className="produit-texte">“Un monde surréaliste rendu vivant par des œuvres d'art peintes à la main.”</span>
+                </div>
 
-                    Quand tout semble désespéré, un enfant est réveillé par les chuchotements de la dernière feuille, attiré dans un monde imaginaire. Unleaving invite le joueur dans un jeu de plateforme et de réflexion axé sur la narration, où chaque défi se déroule dans un paysage peint à la main, explorant les métaphores et les frontières insaisissables de la perception et du sens.
+                <div className="produit-description">
+                    <span className="produit-texte">
+                        UNLEAVING est un jeu de plateforme captivant. Les énigmes et les défis basés sur les compétences sont étroitement intégrés dans la trame de l'histoire, permettant une expérience immersive. UNLEAVING est un jeu de plateforme captivant. Les énigmes et les défis basés sur les compétences sont étroitement intégrés dans la trame de l'histoire, permettant une expérience immersive.
                     </span>
                 </div>
             </div>
         </div>
-    )
+    );
 }
